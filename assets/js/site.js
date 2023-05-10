ffb1908b143c434d64286444640388f42c0d26d9
James-H <james@cloudcannon.com>
2023-05-10 15:41:19 +1200

    fix/test: adds darkmode

--- assets/js/site.js
+++ assets/js/site.js
@@ -2,4 +2,46 @@ import Alpine from 'alpinejs';
 
 window.Alpine = Alpine;
 
+// Alpine.data('dark-mode', () => ({
+//     mode: 'light',
+//     toggle() {
+//         this.mode = this.mode === 'light' ? 'dark' : 'light';
+//     },
+// }));
+
+
+
 Alpine.start();
+
+
+var modeButton = document.getElementById('js-dark-mode');
+var modeLabel = document.getElementById('js-mode-label');
+var mode = localStorage.getItem('mode');
+
+
+function changeMode() {
+	document.body.classList.toggle('dark');
+
+	if (document.body.classList.contains('dark')) {
+		modeLabel.innerText = 'Light Mode'
+		localStorage.setItem('mode', 'dark');
+	} else {
+		modeLabel.innerText = 'Dark Mode'
+		localStorage.setItem('mode', 'light');
+	}
+}
+
+function checkMode() {
+	if (mode) {
+		if (mode == 'dark') {
+			document.body.classList.add('dark');
+			modeLabel.innerText = 'Light Mode';
+		} else {
+			document.body.classList.remove('dark');
+		}
+	}
+}
+
+checkMode();
+
+modeButton.addEventListener('click', changeMode);
