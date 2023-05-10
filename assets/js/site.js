4ae19ca98dca54b099ea52d2d15d41824fb9b2d9
James-H <james@cloudcannon.com>
2023-05-10 17:53:11 +1200

    feat: adds dark mode

--- assets/js/site.js
+++ assets/js/site.js
@@ -14,28 +14,25 @@ window.Alpine = Alpine;
 Alpine.start();
 
 
-var modeButton = document.getElementById('js-dark-mode');
-var modeLabel = document.getElementById('js-mode-label');
-var mode = localStorage.getItem('mode');
+const modeButton = document.getElementById('js-dark-mode');
+const mode = localStorage.getItem('mode');
 
 
 function changeMode() {
 	document.body.classList.toggle('dark');
 
 	if (document.body.classList.contains('dark')) {
-		modeLabel.innerText = 'Light Mode'
 		localStorage.setItem('mode', 'dark');
 	} else {
-		modeLabel.innerText = 'Dark Mode'
 		localStorage.setItem('mode', 'light');
 	}
 }
 
 function checkMode() {
+    console.log(mode, 'mode')
 	if (mode) {
 		if (mode == 'dark') {
 			document.body.classList.add('dark');
-			modeLabel.innerText = 'Light Mode';
 		} else {
 			document.body.classList.remove('dark');
 		}
