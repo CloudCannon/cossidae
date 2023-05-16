cfef10a9cec85b76410336be300b057206f63e74
James-H <james@cloudcannon.com>
2023-05-17 08:51:29 +1200

    feat: adds user custom themes

--- assets/js/site.js
+++ assets/js/site.js
@@ -2,43 +2,21 @@ import Alpine from 'alpinejs';
 
 window.Alpine = Alpine;
 
-// Alpine.data('dark-mode', () => ({
-//     mode: 'light',
-//     toggle() {
-//         this.mode = this.mode === 'light' ? 'dark' : 'light';
-//     },
-// }));
-
-
-
 Alpine.start();
 
 
 const modeButton = document.getElementById('js-dark-mode');
-const mode = localStorage.getItem('mode');
+const body = document.body;
 
 
-function changeMode() {
-	document.body.classList.toggle('dark');
-
-	if (document.body.classList.contains('dark')) {
-		localStorage.setItem('mode', 'dark');
-	} else {
-		localStorage.setItem('mode', 'light');
-	}
+function toggleMode() {
+	const mode = localStorage.getItem('mode') || 'light';
+	const isDark = mode === 'dark';
+	body.classList.toggle('dark', isDark);
+	body.classList.toggle('light', !isDark);
+	localStorage.setItem('mode', isDark ? 'light' : 'dark');
 }
+  
+toggleMode(); 
 
-function checkMode() {
-    console.log(mode, 'mode')
-	if (mode) {
-		if (mode == 'dark') {
-			document.body.classList.add('dark');
-		} else {
-			document.body.classList.remove('dark');
-		}
-	}
-}
-
-checkMode();
-
-modeButton.addEventListener('click', changeMode);
+modeButton.addEventListener('click', toggleMode);
